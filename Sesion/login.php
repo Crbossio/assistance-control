201b1234753c65136fd85a4d94d8525aee873716
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-03 02:09:39 -0500

    Update login.php

--- Sesion/login.php
+++ Sesion/login.php
@@ -27,8 +27,12 @@ else
  $message = "Login failed";
     array_push($response,array("code"=>$code,"message"=>$message));
   echo json_encode(array("server_response"=>$response));
+ <script src="js/jquery-3.1.0.min.js"></script>
+ <script type="text/javascript">
   alert("Por favor, ingrese usuario o contraseña validos");
-
+ </script>
+ 
+  //header('Location: index.php');
 
 }
 
