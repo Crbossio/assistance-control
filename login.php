570f0365a8923d0021a2ddb30cab0ce2375bd21b
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-10 13:24:22 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+$email = $_REQUEST["email"];
+$pass = $_REQUEST["password"];
+require "connection.php";
+
+// Realizar una consulta MySQL
+$query = "SELECT * FROM userinfo WHERE email LIKE '$email' AND password LIKE '$pass'"; 
+$result = mysqli_query($conn, $query) or die("Failed: " . mysqli_error()); 
+if(mysqli_num_rows($result)>0)
+{
+ $response = array();
+ $code = "login_true";
+ $row = mysqli_fetch_array($result);
+ //first param of the row is name, thats why name is retrieved here
+ $name = $row[1];
+ $message = "Login success and Welcome ".$name;
+    array_push($response,array("code"=>$code,"message"=>$message));
+    echo json_encode(array("server_response"=>$response));
+    header('Location: nuevoindex.html');
+
+}
+else
+{
+ $response = array();
+ $code = "login_false";
+ $message = "Login failed";
+    array_push($response,array("code"=>$code,"message"=>$message));
+  echo json_encode(array("server_response"=>$response));
+  header('Location: index.html');
+
+}
+
+ mysqli_close($conn);
+
+
+?>
---
+++ regprof.php
@@ -0,0 +1,44 @@
+<?php
+$db_host="gpstraking.cvsno6ctfz1z.us-west-2.rds.amazonaws.com";
+$db_user="admin";
+$db_password="admin123";
+$db_name="USERSDB";
+
+
+$db_table_name="userinfo";
+
+   $db_connection = mysql_connect($db_host, $db_user, $db_password);
+
+if (!$db_connection) {
+    die('No se ha podido conectar a la base de datos');
+}
+
+$subs_name = utf8_decode($_POST['nombre']);
+$subs_cedula = utf8_decode($_POST['cedula']);
+$subs_email = utf8_decode($_POST['email']);
+$subs_contraseña = utf8_decode($_POST['contraseña']);
+
+$resultado=mysql_query("SELECT * FROM ".$db_table_name." WHERE Email = '".$subs_email."'", $db_connection);
+
+if (mysql_num_rows($resultado)>0)
+{
+
+header('Location: Fail.html');
+
+} else {
+    
+    $insert_value = 'INSERT INTO `' . $db_name . '`.`'.$db_table_name.'` (`id` , `name` , `email`,`password`) VALUES ("' . $subs_cedula . '", "' . $subs_name . '", "' . $subs_email . '",  "' . $subs_contraseña . '"  )';
+
+mysql_select_db($db_name, $db_connection);
+$retry_value = mysql_query($insert_value, $db_connection);
+
+if (!$retry_value) {
+   die('Error: ' . mysql_error());
+}
+    
+header('Location: successprof.html');
+}
+
+mysql_close($db_connection);
+        
+?>
