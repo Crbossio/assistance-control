ca6214427bb0c8bf78d3084479e53c7e18467153
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-16 17:00:00 -0500

    Update nuevoindex.php

--- nuevoindex.php
+++ nuevoindex.php
@@ -194,7 +194,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 												<div class="clearfix"> 
 												<textarea id="Mensaje" name="Mensaje" placeholder="Mensaje..." required=""></textarea>
 													</div>
-												<input type="submit" value="Submit">
+												<button  type="submit">Enviar</button>
 											</form>
 										</div>
 									</div>
