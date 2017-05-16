769bfe0fec3e8d51d306234e57fb4a505a8c8705
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-16 16:05:13 -0500

    Update login.php

--- login.php
+++ login.php
@@ -25,10 +25,31 @@ else
  $response = array();
  $code = "login_false";
  $message = "Login failed";
- echo "<script>alert('Login failed.. try again');location.href ='javascript:history.back()';</script>";
+ //echo "<script>alert('Login failed.. try again');location.href ='javascript:history.back()';</script>";
     array_push($response,array("code"=>$code,"message"=>$message));
   echo json_encode(array("server_response"=>$response));
   header('Location: index.php');
+
+}
+if(mysqli_num_rows($result)>0)
+{
+ $response = array();
+ $code = "login_true";
+ $row = mysqli_fetch_array($result);
+ //first param of the row is name, thats why name is retrieved here
+ $name = $row[1];
+ $message = "Login success and Welcome ".$name;
+    array_push($response,array("code"=>$code,"message"=>$message));
+    echo json_encode(array("server_response"=>$response));
+    header('Location: nuevoindex.php');
+
+}
+else
+{
+ 
+ echo "<script>alert('Login failed.. try again');location.href ='javascript:history.back()';</script>";
+    array_push($response,array("code"=>$code,"message"=>$message));
+  
 
 }
 
