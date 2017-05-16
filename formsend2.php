757e22b4e4cfd013767db8523e920fbe9f3a532c
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-16 18:12:30 -0500

    Add files via upload

---
+++ formsend2.php
@@ -0,0 +1,22 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Formulario enviado</title>
+<link href="estilos.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div class="group">
+  <form action="registro.php" method="POST">
+  <h2><em>Formulario enviado!</em></h2>
+
+  </form>
+  <form action="index.php" method="POST">
+  <center> <input class="form-btn" name="submit" type="submit" value="Aceptar" /></center>
+  </form>
+</div>
+</body>
+</html>
