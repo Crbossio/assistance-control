7ca11c04f51e2e854d498d9c783ae69aac4dfe64
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-16 18:01:50 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -41,7 +41,7 @@ $mail->Password = "asistencia"; // Contraseña del correo
 
 if ($mail->Send()){
 echo "<script>alert('Formulario enviado exitosamente, le responderemos lo más pronto posible.');location.href ='javascript:history.back()';</script>";
-header('Location: index.html');
+header('Location: index.php');
 }else
 {
 echo "<script>alert('Error al enviar el formulario');location.href ='javascript:history.back()';</script>";
