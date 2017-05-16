37cb9f4e4a52c321859b7a4c37d8368c27aaea60
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-15 19:06:03 -0500

    Update conexion.php

--- controlador/conexion.php
+++ controlador/conexion.php
@@ -1,6 +1,5 @@
 <?php
 //$conexion = mysql_connect('127.0.0.1', 'avalos12', 'avalos12');
-$conexion = mysql_connect('gpstraking.cvsno6ctfz1z.us-west-2.rds.amazonaws.com', 'admin', 'admin123');
-mysql_select_db('asistencia', $conexion);
+$conn = new mysqli("gpstraking.cvsno6ctfz1z.us-west-2.rds.amazonaws.com", "admin", "admin123", "asistencia"); 
 
 ?>
