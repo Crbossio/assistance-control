0fc76366a74e0f82edf8f2508141ba1f277576e6
Crbossio <cristianbossioolaya@hotmail.com>
2017-05-16 22:09:45 -0500

    Update regprof1.php

--- regprof1.php
+++ regprof1.php
@@ -7,7 +7,7 @@ $db_name="USERSDB";
 
 $db_table_name="userinfo";
 
-   $db_connection = mysql_connect($db_host, $db_user, $db_password);
+   $db_connection = new mysqli($db_host, $db_user, $db_password, $db_name);
 
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
@@ -18,22 +18,22 @@ $subs_cedula = utf8_decode($_POST['cedula']);
 $subs_email = utf8_decode($_POST['email']);
 $subs_contraseña = utf8_decode($_POST['contraseña']);
 
-$resultado=mysql_query("SELECT * FROM ".$db_table_name." WHERE Email = '".$subs_email."'", $db_connection);
+$resultado=mysqli_query("SELECT * FROM ".$db_table_name." WHERE Email = '".$subs_email."'", $db_connection);
 
-if (mysql_num_rows($resultado)>0)
+if (mysqli_num_rows($resultado)>0)
 {
 
 header('Location: Fail.html');
 
 } else {
     
-    $insert_value = 'INSERT INTO '' . $db_name . ''.''.$db_table_name.'' ('id' , 'name' , 'email','password') VALUES ("' . $subs_cedula . '", "' . $subs_name . '", "' . $subs_email . '",  "' . $subs_contraseña . '"  )';
+ $insert_value = 'INSERT INTO '' . $db_name . ''.''.$db_table_name.'' ('id' , 'name' , 'email','password') VALUES ("' . $subs_cedula . '", "' . $subs_name . '", "' . $subs_email . '",  "' . $subs_contraseña . '"  )';
 
-mysql_select_db($db_name, $db_connection);
-$retry_value = mysql_query($insert_value, $db_connection);
+//mysql_select_db($db_name, $db_connection);
+$retry_value = mysqli_query($db_connection, $insert_value);
 
 if (!$retry_value) {
-   die('Error: ' . mysql_error());
+   die('Error: ' . mysqli_error());
 }
     
 header('Location: successprof.php');
